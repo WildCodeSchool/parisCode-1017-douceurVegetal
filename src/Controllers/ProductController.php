79583d48d16087506018faa0df3b1b09c31a9d0a
Sara Lima <clima.sara@gmail.com>
2017-11-24 11:56:59 +0100

    fiches produits depuis la bd

--- src/Controllers/ProductController.php
+++ src/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace DouceurVegetale\Controllers;
 
 use DouceurVegetale\Model\Repository\ProductManager;
 
+
 /* add, edit, delete */
 
 /**
@@ -13,32 +14,14 @@ use DouceurVegetale\Model\Repository\ProductManager;
 class ProductController extends Controller
 {
     /**
-     * Product functions
+     * Products show
      */
 
-    public function showDataProducts()
+    public function addProducts()
     {
-        if ($_SERVER['REQUEST_METHOD'] == "POST") {
-            $product_name = $_POST['name'];
-            $description = $_POST['description'];
-            $categorie = $_POST['categorie'];
-            $image = $_POST['image'];
 
 
-            $manager = new ModelManager();
-            $manager->add($product_name, $description, $categorie);
-            $db = mysqli_connect(SERVER, USER, PASSWORD, BD);
-            $resultat = mysqli_query($db, 'SELECT * FROM products');
-
-            while ($resultBdd = mysqli_fetch_all($resultat)) {
-                echo $_POST['name'];
-                echo $_POST['description'];
-                echo $_POST['categorie'];
-
-            }
-
-            return $this->twig->render('user/home.html.twig');
-        }
     }
+
 
 }
