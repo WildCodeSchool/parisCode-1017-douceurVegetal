64ceea73dbbf7942c20408a3256c6f77640c4b2f
amandinedargon <amandine.dargon@gmail.com>
2017-12-06 16:49:26 +0100

    Mise en place hover logo nav + target blank sur cnil onit message for your changes. Lines starting

--- src/Controllers/DefaultController.php
+++ src/Controllers/DefaultController.php
@@ -1,50 +1,47 @@
 <?php
-
     namespace DouceurVegetale\Controllers;
-
+    use DouceurVegetale\Model\Repository\CategoriesManager;
     use DouceurVegetale\Model\Repository\ContactManager;
     use DouceurVegetale\Model\Repository\ShopinfosManager;
     use DouceurVegetale\Model\Repository\UserManager;
     use DouceurVegetale\Model\Repository\ProductManager;
     use DouceurVegetale\Model\Repository\HomepageManager;
-
-
     /**
      * Class DefaultController
      * @package DouceurVegetale\Controllers
      */
     class DefaultController extends Controller
     {
-
         /**
          * Render index
          */
         public function indexAction()
         {
-
-        $shopinfosManager = new ShopinfosManager();
-        $homepageManager = new HomepageManager();
-        $homepage = $homepageManager->getAllHomepage();
-        $shopinfos = $shopinfosManager->getAllShopinfos();
-        return $this->twig->render('user/home.html.twig', array(
-            'homepage' => $homepage,
-            'shopinfos' => $shopinfos
-        ));
-    }
-
-
-    /**
-     * Render products page
-     */
-    public function showProductsAction()
-    {
-        $productManager = new ProductManager();
-        $products = $productManager->getAllProducts();
-        $shopinfosManager = new ShopinfosManager();
-        $shopinfos = $shopinfosManager->getAllShopinfos();
-        return $this->twig->render('user/products.html.twig', array_merge(array('products' => $products), array('shopinfos' => $shopinfos)));
-    }
-
+            $shopinfosManager = new ShopinfosManager();
+            $homepageManager = new HomepageManager();
+            $homepage = $homepageManager->getAllHomepage();
+            $shopinfos = $shopinfosManager->getAllShopinfos();
+            return $this->twig->render('user/home.html.twig', array(
+                'homepage' => $homepage,
+                'shopinfos' => $shopinfos
+            ));
+        }
+        /**
+         * Render products page
+         */
+        public function showProductsAction()
+        {
+            $productManager = new ProductManager();
+            $products = $productManager->getAllProducts();
+            $shopinfosManager = new ShopinfosManager();
+            $shopinfos = $shopinfosManager->getAllShopinfos();
+            $productSemaine = $productManager->getProductWeek();
+            return $this->twig->render('user/products.html.twig', array(
+                'products' => $products,
+                'shopinfos' => $shopinfos,
+                'productWeek' => $productSemaine
+            ));
+        }
         /**
          * Render values page
          */
@@ -55,7 +52,29 @@
             return $this->twig->render('user/values.html.twig', array(
                 'shopinfos' => $shopinfos));
         }
-
+        /**
+         * Swiftmailer
+         */
+        public function sendMail($form)
+        {
+            // Create the Transport
+            $transport = (new \Swift_SmtpTransport('smtp.mail.fr', 465, 'ssl'))
+                ->setUsername('douceurvegetale@mail.fr')
+                ->setPassword('DouceurVegetale');
+            // Create the Mailer using your created Transport
+            $mailer = new \Swift_Mailer($transport);
+            // Create a message
+            $message = (new \Swift_Message($form['object'], 'text/html'))
+                ->setFrom(['douceurvegetale@mail.fr' => 'Douceur Végétale'])
+                ->setTo(['douceurvegetale@mail.fr' => 'Douceur Végétale'])
+                ->setBody(
+                    $this->twig->render('user/mail.html.twig', array(
+                        'form' => $form
+                    )), 'text/html');
+            // Send the message
+            $result = $mailer->send($message, $failures);
+            return $this->twig->render('user/success_contact.html.twig');
+        }
         /**
          * Render contact page
          */
@@ -67,10 +86,10 @@
                     $errors['name'] = "Merci de bien vouloir renseigner votre nom";
                 }
                 if (empty($_POST['email'])) {
-                    $errors['email'] = "Merci de bien vouloir renseigner une adresse mail";
+                    $errors['email'] = "Merci de bien vouloir renseigner votre adresse mail";
                 }
                 if (empty($_POST['message'])) {
-                    $errors['message'] = "Merci de bien vouloir renseigner un message";
+                    $errors['message'] = "Merci de bien vouloir renseigner votre message";
                 }
                 if (count($errors) > 0) {
                     return $this->twig->render('user/contact.html.twig', array(
@@ -87,48 +106,4 @@
                 'shopinfos' => $shopinfos
             ));
         }
-
-
-        public function showDataUser()
-        {
-
-        }
-
-        public function editData()
-        {
-            return $this->twig->render('user/modify.html.twig');
-        }
-
-        public function deleteData()
-        {
-            return $this->twig->render('delete/.html.twig');
-        }
-
-
-    /**
-     * Swiftmailer
-     */
-    public function sendMail($form)
-    {
-        // Create the Transport
-        $transport = (new \Swift_SmtpTransport('smtp.mail.fr', 465, 'ssl'))
-            ->setUsername('douceurvegetale@mail.fr')
-            ->setPassword('DouceurVegetale');
-        // Create the Mailer using your created Transport
-        $mailer = new \Swift_Mailer($transport);
-        // Create a message
-        $message = (new \Swift_Message($form['object'], 'text/html'))
-            ->setFrom(['douceurvegetale@mail.fr' => 'Douceur Végétale'])
-            ->setTo(['douceurvegetale@mail.fr' => 'Douceur Végétale'])
-            ->setBody(
-                $this->twig->render('user/mail.html.twig', array(
-                    'form' => $form
-                )), 'text/html');
-        // Send the message
-        $result = $mailer->send($message, $failures);
-        return $this->twig->render('user/success_contact.html.twig');
     }
-
-
-
-}
