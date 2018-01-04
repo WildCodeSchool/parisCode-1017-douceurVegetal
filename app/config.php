81f7fc2820e7201f9b7aec60ef60046a6062a276
Emeline Ancel-Pirouelle <contact@emelineancelpirouelle.com>
2018-01-04 12:19:05 +0100

    config.php updated

--- app/config.php
+++ app/config.php
@@ -1,7 +1,7 @@
 <?php
 
 define('SERVER', 'sql11.freemysqlhosting.net');
-define('USER', 'sql11209110');
-define('PASSWORD', 'prGLmNSCwn');
-define('DB', 'sql11209110');
+define('USER', 'sql11214001');
+define('PASSWORD', 'd2mg5QzaSn');
+define('DB', 'sql11214001');
 define('PORT', '3306');
